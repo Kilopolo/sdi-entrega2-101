1ef14511dff07681c604cd551fe3cadd5bdc4418
Pablo Diaz Rubio <71383208+UO271245@users.noreply.github.com>
2022-05-12 13:06:46 +0200

    d

--- sdi-entrega2-test-101/src/main/java/socialnetwork/pageobjects/PO_PrivateView.java
+++ sdi-entrega2-test-101/src/main/java/socialnetwork/pageobjects/PO_PrivateView.java
@@ -1,6 +1,8 @@
 package socialnetwork.pageobjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class PO_PrivateView extends PO_NavView {
 
@@ -18,9 +20,27 @@ public class PO_PrivateView extends PO_NavView {
     public static void loginAPI(WebDriver driver, String email, String password, String check) {
 
         // Rellenamos el formulario
-        PO_LoginView.fillLoginForm(driver, email, password);
+        fillLoginForm(driver, email, password);
         // COmprobamos que entramos en la pagina privada
         checkElementBy(driver, "id", check);
     }
 
+    private static void fillLoginForm(WebDriver driver, String emailp, String passwordp) {
+        WebElement email = driver.findElement(By.name("email"));
+        email.click();
+        email.clear();
+        email.sendKeys(emailp);
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.click();
+        password.clear();
+        password.sendKeys(passwordp);
+
+        // Pulsar el boton de Login.
+        By boton = By.className("boton-login");
+//        By boton = By.className("btn");
+
+        driver.findElement(boton).click();
+                System.out.println(boton.toString());
+    }
 }
